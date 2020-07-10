3650fcbcf5eab5088311e9301e30800a56556763
dilless <bohuang1994@gmail.com>
2020-07-10 09:49:59 +0800

    test

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-class Solution:
-    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
-        global ans
-
-        def dfs(node, prev_total):
-            if node is None:
-                if prev_total == sum:
-                    ans = True
-                return
-            cur_total = prev_total + node.val
-            dfs(node.left, cur_total)
-            dfs(node.right, cur_total)
-
-        dfs(root, 0)
-        return ans
-
-
-if __name__ == '__main__':
-    so
+from celery import Celery
+app = Celery('hello', broker='redis://localhost:6379/0')
+@app.task
+def hello():
+    return 'hello world'
